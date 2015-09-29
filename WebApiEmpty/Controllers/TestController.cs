ac02e584d0076c3d989ea8557fe1c435aaf7cac6
Michal Szczepaniak <mr.szczepaniak@gmail.com>
2015-09-29 05:39:14 +0200

    Play with date

--- WebApiEmpty/Controllers/TestController.cs
+++ WebApiEmpty/Controllers/TestController.cs
@@ -1,12 +1,28 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Web.Http;
 
 namespace WebApiEmpty.Controllers
 {
     public class TestController : ApiController
     {
-        public int Get(int id)
+        public class Sample
         {
-            return id;
+            public int ID { get; set; }
+
+            public string Description { get; set; }
+
+            public DateTime Date { get; set; }
+        }
+
+        public IHttpActionResult Get()
+        {
+            var sample = new Sample {ID=10, Description = "bla", Date = DateTime.Now};
+            return Ok(sample);
+        }
+
+        public IHttpActionResult Post()
+        {
+            return Ok();
         }
     }
 }
